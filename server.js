86ea3e522bc5b6a5bdf4146ff47211279a8fa3b7
velafarq <vela.mrdjen@gmail.com>
2018-08-22 10:44:36 -0500

    Add CORS

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
